bd8b6a524ac132c14683b4dee0c8ada569144e82
zibro <jgh6272@gmail.com>
2025-06-25 23:14:03 +0900

    #7 chore: AndroidApplicationConventionPlugin 정의

---
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,30 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension
+import com.android.build.gradle.BaseExtension
+import com.zibro.gyeonggilocationcurrencyapp.configureKotlinAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
+
+/**
+ * android library 모듈에 적용되는 공통 설정을 정의
+ */
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.android.application")
+            apply(plugin = "org.jetbrains.kotlin.android")
+            apply(plugin = "nowinandroid.android.lint")
+            apply(plugin = "com.dropbox.dependency-guard")
+
+            extensions.configure<ApplicationExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 35
+                @Suppress("UnstableApiUsage")
+                testOptions.animationsDisabled = true
+            }
+        }
+    }
+}
